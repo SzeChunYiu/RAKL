0b3e116b8ccb6018b34c85f72930f5c79bd59d80
Sze Chun YIU <52784502+SzeChunYiu@users.noreply.github.com>
2026-08-09 00:25:47 +0200

    Expose RAKL core API

---
+++ src/rakl/__init__.py
@@ -0,0 +1,25 @@
+from .core import (
+    Authority,
+    Context,
+    Discriminator,
+    KnowledgeFiber,
+    Projection,
+    Relation,
+    Relationship,
+    compare_contexts,
+    rank_discriminators,
+    semantic_gain,
+)
+
+__all__ = [
+    "Authority",
+    "Context",
+    "Discriminator",
+    "KnowledgeFiber",
+    "Projection",
+    "Relation",
+    "Relationship",
+    "compare_contexts",
+    "rank_discriminators",
+    "semantic_gain",
+]
