06baf38132be62cc8dae356071a824249d3e05bc
Sze Chun YIU <52784502+SzeChunYiu@users.noreply.github.com>
2026-08-09 02:43:19 +0200

    Expose transactional promotion API

--- src/rakl/__init__.py
+++ src/rakl/__init__.py
@@ -10,15 +10,29 @@ from .core import (
     rank_discriminators,
     semantic_gain,
 )
+from .promotion import (
+    CheckConclusion,
+    PromotionDecision,
+    PromotionGate,
+    PromotionPacket,
+    PromotionVerdict,
+    RequiredCheck,
+)
 
 __all__ = [
     "Authority",
+    "CheckConclusion",
     "Context",
     "Discriminator",
     "KnowledgeFiber",
     "Projection",
+    "PromotionDecision",
+    "PromotionGate",
+    "PromotionPacket",
+    "PromotionVerdict",
     "Relation",
     "Relationship",
+    "RequiredCheck",
     "compare_contexts",
     "rank_discriminators",
     "semantic_gain",
